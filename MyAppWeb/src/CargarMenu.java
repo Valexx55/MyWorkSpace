a69fe98166b50769e7c886035f5bd2ac84b35bfc
Val <valexx55@gmail.com>
2016-07-16 12:18:30 +0200

    Commit inicial

---
+++ MyAppWeb/src/CargarMenu.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CargarMenu
+ */
+@WebServlet("/CargarMenu")
+public class CargarMenu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CargarMenu() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = null;
+		
+		System.out.println(request.getParameter("num"));
+		
+				
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
+	
+		
+		out = response.getWriter();
+		out.println("MENSAJE INICIAL");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
